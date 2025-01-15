716f6d75cac58ba4c6a71383e4fa7f5495d04e4a
Bruno-Filadelfo <bruno.filadelfo@outlook.com>
2025-01-15 03:07:57 -0300

    aprendendo switch case

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+if (letra == "a") {
+    Console.WriteLine("vogal");
+}
+
+else if (letra == "e") {
+    Console.WriteLine("vogal");
+}
+
+else if (letra == "i") {
+    Console.WriteLine("vogal");
+}
+
+else if (letra == "o") {
+    Console.WriteLine("vogal");
+}
+
+else if (letra == "u") {
+    Console.WriteLine("vogal");
+}
+
+else {
+    Console.WriteLine("Não é vogal!");
+}
+
+////
+
+Console.WriteLine("Digite uma letra!");
+string letraa = Console.ReadLine();
+
+if (letraa == "a" ||
+    letraa == "e" ||
+    letraa == "i" ||
+    letraa == "o" ||
+    letraa == "u") {
+        Console.WriteLine("vogal");
+    }
+
+else {
+    Console.WriteLine("Não é vogal");
+}   
+
+
+////
+
+switch (letra) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    Console.WriteLine("vogal");
+    break;
+
+    default:
+    Console.WriteLine("Não é vogal");
+    break;
+}
+
+
+
+
+
