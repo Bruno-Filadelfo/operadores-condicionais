356d51655ed7c6dbd3660c5ac937942eaf93a8ea
Bruno-Filadelfo <bruno.filadelfo@outlook.com>
2025-01-19 04:34:15 -0300

    operador not

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿bool choveu = true;
+bool estaTarde = true;
+
+if (choveu && estaTarde) {
+    Console.WriteLine("Vou pedalar");
+
+}
+
+else {
+    Console.WriteLine("Vou pedalar um outro dia!");
+}
